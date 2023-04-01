b278cbb12511784fbbedfb40991c671340c16058
Otir <45825931+otirmesilati@users.noreply.github.com>
2023-04-01 13:06:49 +0300

    First routing patch
    Adding the base for the main components, and a bit of the same for the containers

---
+++ webgpt_jsm/src/components/index.js
@@ -0,0 +1,5 @@
+export { defualt as Article } from './article/Article'; 
+export { defualt as Brand } from './barnd/Brand'; 
+export { defualt as CTA } from './cta/CTA'; 
+export { defualt as Feature } from './feature/Feature'; 
+export { defualt as Navbar } from './navbar/Navbar'; 
